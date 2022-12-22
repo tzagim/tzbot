f3902e5853ba907f68e8a49e5d221620b354305d
tzagim <2285958+tzagim@users.noreply.github.com>
2022-12-22 11:44:10 +0200

    Update delete_join.py

--- tzbot/modules/delete_join.py
+++ tzbot/modules/delete_join.py
@@ -1,15 +1,14 @@
 from telegram.ext import CallbackContext
 from telegram.update import Update
 from telegram.ext import MessageHandler, Filters
-from tzbot import API_KEY, dispatcher, updater
+from tzbot import dispatcher, updater
 
 def joinleft(update: Update, context: CallbackContext):
     context.bot.delete_message(chat_id=update.message.chat_id,message_id=update.message.message_id)
 
 def massagedel():
     updater.start_polling()
-    dp = updater.dispatcher
-    dp.add_handler(MessageHandler(Filters.status_update.new_chat_members,joinleft))
-    dp.add_handler(MessageHandler(Filters.status_update.left_chat_member,joinleft))
+    ud = updater.dispatcher
+    ud.add_handler(MessageHandler(Filters.status_update.new_chat_members | Filters.status_update.left_chat_member ,joinleft))
 
 massagedel()
