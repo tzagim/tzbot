1b1f0a893c00fd21bf61d95158cfc7eee9255d09
tzagim <2285958+tzagim@users.noreply.github.com>
2022-12-31 20:46:26 +0200

    Update misc.py

--- tzbot/modules/misc.py
+++ tzbot/modules/misc.py
@@ -1,8 +1,12 @@
 from telegram import Bot, ParseMode, Update
 from telegram.ext import Filters, MessageHandler
+from tzbot import FROM_CHATS, OWNER_ID, TO_CHATS, dispatcher, LANG
+from tzbot.langdict import en, he
 
-from tzbot import FROM_CHATS, OWNER_ID, TO_CHATS, dispatcher
-
+if LANG == 'he':
+  LANG = he
+else:
+  LANG = en
 
 def get_id(update, context):
     message = update.effective_message  # type: Optional[Message]
@@ -13,47 +17,36 @@ def get_id(update, context):
         ):  # Replied message is a forward from a user
             sender = message.reply_to_message.forward_from
             forwarder = message.reply_to_message.from_user
-            message.reply_text(
-                "השולח המקורי, {}, המזהה שלו (ID) הוא `{}`. \n"
-                "המעביר, {}, הוא בעל מזהה (ID) `{}`.".format(
-                    sender.first_name, sender.id, forwarder.first_name, forwarder.id
-                ),
-                parse_mode=ParseMode.MARKDOWN,
+            message.reply_text(LANG.get('Forward_From').format(sender.first_name, sender.id, forwarder.first_name, forwarder.id),
+                               parse_mode=ParseMode.MARKDOWN,
             )
         elif (
             message.reply_to_message.forward_from_chat
         ):  # Replied message is a forward from a channel
             channel = message.reply_to_message.forward_from_chat
             forwarder = message.reply_to_message.from_user
-            message.reply_text(
-                "הערוץ, {}, הוא בעל מזהה (ID) `{}`. \n"
-                "המעביר, {}, הוא בעל מזהה (ID) `{}`.".format(
-                    channel.title, channel.id, forwarder.first_name, forwarder.id
-                ),
-                parse_mode=ParseMode.MARKDOWN,
+            message.reply_text(LANG.get('Forward_From_Chat').format(channel.title, channel.id, forwarder.first_name, forwarder.id),
+                               parse_mode=ParseMode.MARKDOWN,
             )
 
         else:
             user = (
                 message.reply_to_message.from_user
             )  # Replied message is a message from a user
-            message.reply_text(
-                "{} בעל מזהה (ID) `{}`.".format(user.first_name, user.id),
-                parse_mode=ParseMode.MARKDOWN,
+            message.reply_text(LANG.get('From_User').format(user.first_name, user.id),
+                               parse_mode=ParseMode.MARKDOWN,
             )
 
     else:
         chat = update.effective_chat
 
         if chat.type == "private":  # Private chat with the bot
-            message.reply_text(
-                "המזהה שלך (ID) הוא `{}`.".format(chat.id), parse_mode=ParseMode.MARKDOWN
-            )
+            message.reply_text(LANG.get('Your_Id').format(chat.id),
+                               parse_mode=ParseMode.MARKDOWN)
 
         else:  # Group chat where the bot is a member
-            message.reply_text(
-                "המזהה של הקבוצה (ID) הוא `{}`.".format(chat.id),
-                parse_mode=ParseMode.MARKDOWN,
+            message.reply_text(LANG.get('Group_Id').format(chat.id),
+                               parse_mode=ParseMode.MARKDOWN,
             )
 
 
