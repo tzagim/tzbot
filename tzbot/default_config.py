dc3a6599efc9c5a427dd7c8c5485e88aab80acd4
tzagim <2285958+tzagim@users.noreply.github.com>
2023-04-15 14:48:20 +0300

    Add files via upload

--- tzbot/default_config.py
+++ tzbot/default_config.py
@@ -15,12 +15,12 @@ class Config(object):
     REMOVE_TAG = False
 
     # REQUIRED
-#    API_KEY = "1234567890:Abcdef1234567890GHIJ"  # API key obtained from BotFather
-#    OWNER_ID = "1234567890"  # If you dont know, run the bot and do /id in your private chat with the bot
+    API_KEY = "1234567890:Abcdef1234567890GHIJ"  # API key obtained from BotFather
+    OWNER_ID = "1234567890"  # If you dont know, run the bot and do /id in your private chat with the bot
 
     # FOR AUTOMATICALLY FORWARDING MESSAGES
-#    FROM_CHATS = [-1001234567890]  # List of chat id's to forward messages from
-#    TO_CHATS = [-1001234567890]  # List of chat id's to forward messages to
+    FROM_CHATS = [-1001234567890]  # List of chat id's to forward messages from
+    TO_CHATS = [-1001234567890]  # List of chat id's to forward messages to
     
     # FOR DELELTE MESSAGES AFTER X MINUTS
     GROUPS_TO_DELETE = [-1001234567890] # List of chat id's to delete messages from
@@ -31,7 +31,7 @@ class Config(object):
 
     # Supported languages
     # en, he
-#    LANG = 'en'
+    LANG = 'en'
 
 class Production(Config):
     LOGGER = False
