2f56e1e325df76a69ed17ef3595bd1d8dc7c807d
tzagim <2285958+tzagim@users.noreply.github.com>
2024-06-05 23:15:37 +0300

    Start for all

--- tzbot/modules/default.py
+++ tzbot/modules/default.py
@@ -9,15 +9,22 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat = update.effective_chat
     message = update.effective_message
     user = update.effective_user
-    lang_code = message.from_user.language_code
+    lang_code = LANG
+    try:
+        lang_code = message.from_user.language_code
+    except (AttributeError, KeyError):
+        pass
     if not (chat and message and user):
         return
 
     if chat.type == "private":
-        await message.reply_text(
-        strings["pm_start"][lang_code].format(user.first_name, context.bot.first_name),
-            parse_mode=ParseMode.HTML,
-        )
+        if user.id in OWNER_ID:
+            await message.reply_text(
+            strings["pm_start"][lang_code].format(user.first_name, context.bot.first_name),
+                parse_mode=ParseMode.HTML,
+            )
+        else:
+            await message.reply_text(strings["owner_only"][lang_code])
     else:
         await message.reply_text(strings["run_msg"][lang_code])
 
@@ -33,5 +40,5 @@ async def help(update: Update, _):
     else:
         await message.reply_text(strings["pm_help"][lang_code])
 
-bot.add_handler(CommandHandler("start", start, filters=filters.User(OWNER_ID)))
+bot.add_handler(CommandHandler("start", start))
 bot.add_handler(CommandHandler("help", help, filters=filters.User(OWNER_ID)))
