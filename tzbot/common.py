eabf46f0c59ddd6f8c37c74cd45e5984728bc698
tzagim <2285958+tzagim@users.noreply.github.com>
2025-07-15 01:12:53 +0300

    Add id cmd to common.py

--- tzbot/common.py
+++ tzbot/common.py
@@ -30,20 +30,27 @@ def schedule_delete(context: ContextTypes.DEFAULT_TYPE, chat_id: int, message_id
             f" in chat {chat_id} after {delay}s"
         )
 
+def get_lang_code(message):
+    if not message:
+        return LANG
+    try:
+        return message.from_user.language_code
+    except (AttributeError, KeyError):
+        return LANG
+
+def is_owner(user_id):
+    return user_id in OWNER_ID
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat      = update.effective_chat
     message   = update.effective_message
     user      = update.effective_user
-    lang_code = LANG
-    try:
-        lang_code = message.from_user.language_code
-    except (AttributeError, KeyError):
-        pass
+    lang_code = get_lang_code(update.effective_message)
 
     if not (chat and message and user):
         return
 
-    if user.id in OWNER_ID:
+    if is_owner(user.id):
         if chat.type == "private":
             sent = await message.reply_text(
                 strings["pm_start"][lang_code].format(
@@ -63,7 +70,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat      = update.effective_chat
     message   = update.effective_message
-    lang_code = message.from_user.language_code
+    lang_code = get_lang_code(update.effective_message)
+    
     if not (chat and message):
         return
 
@@ -79,10 +87,51 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def id_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat    = update.effective_chat
     message = update.effective_message
+    lang_code = get_lang_code(update.effective_message)
+
     if not (chat and message):
         return
 
-    sent = await message.reply_text(f"Chat ID is: {chat.id}")
+    result = ''
+
+    if chat.is_forum:
+        result += strings['forum_id'][lang_code].format(message.message_thread_id)
+
+    if message.reply_to_message:
+        forward_origin = message.reply_to_message.forward_origin
+
+        if forward_origin:
+            forwarder = message.reply_to_message.from_user
+
+            # Forwarded user
+            if forward_origin.type == 'user':
+                sender = forward_origin.sender_user
+
+                # Check if the user is checking themselves
+                if chat.id == sender.id:
+                    result = ''
+                else:
+                    result += strings["original_sender"][lang_code].format(sender.first_name, sender.id)
+
+            # Forwarded channel
+            elif forward_origin.type == 'channel':
+                channel = forward_origin.chat
+                result += strings["original_channel"][lang_code].format(channel.title, channel.id)
+
+            result += strings['forwarder_user'][lang_code].format(forwarder.first_name if forwarder else 'Unknown', forwarder.id if forwarder else 'Unknown')
+
+    else:
+        # Private chat with the bot
+        if chat.type == 'private':
+            result += strings['your_id'][lang_code].format(chat.id)
+        else:
+            result += strings['chat_id'][lang_code].format(chat.id)
+
+    # Check if result is empty
+    if not result:
+        result = strings["empty_result"][lang_code]
+
+    sent = await message.reply_text(result, parse_mode=ParseMode.MARKDOWN)
 
     if chat.id in DELETE_AFTER:
         schedule_delete(context, chat.id, message.message_id)
@@ -101,11 +150,16 @@ bot.add_handler(
     CommandHandler(
         "help",
         help,
-        filters = filters.User(OWNER_ID)),
+        filters = filters.User(OWNER_ID)
+    ),
     group=0
 )
 
 bot.add_handler(
-    CommandHandler("id", id_cmd),
+    CommandHandler(
+        "id",
+        id_cmd,
+        filters = (filters.User(OWNER_ID) | filters.ChatType.CHANNEL)
+    ),
     group=0
 )
